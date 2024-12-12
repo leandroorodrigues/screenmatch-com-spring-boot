80c3fe97459682d86e641d490fb3f21f5f5ae5f3
Leandro Rodrigues <lrodriguespessoal@gmail.com>
2024-12-11 21:30:57 -0300

    Criando aplicação screenmatch com Spring

---
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=Gilmore+Girls&Season=1&apikey=ce4be4b");
+		System.out.println(json);
+	}
+}
